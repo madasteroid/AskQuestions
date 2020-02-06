5fdfaab4608bf7fdb7a99047e9d68d3fb7145385
madasteroid <prakash.timalsina@selu.edu>
2020-02-06 13:21:15 -0600

    routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule'},
+  {
+    path: 'register',
+    loadChildren: './register/register.module#RegisterPageModule'
+  },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'menu-tabs', loadChildren: './menu-tabs/menu-tabs.module#MenuTabsPageModule' },
+  { path: 'post/:id', loadChildren: './post/post.module#PostPageModule' },  { path: 'user-posts', loadChildren: './user-posts/user-posts.module#UserPostsPageModule' },
+
+
+
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
